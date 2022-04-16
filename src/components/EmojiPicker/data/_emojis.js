98f25aa9e0e196e70910e99bb3559e8a1e49a7db
cetr <coor@coor.top>
2022-04-16 22:44:15 +0800

    update

--- src/components/EmojiPicker/data/_emojis.js
+++ src/components/EmojiPicker/data/_emojis.js
@@ -7,7 +7,7 @@ class Emoji {
     }
 }
 
-const emojiImgPath = "https://www.unpkg.com/halo-comment-yu@latest/cdn/img/emoji";
+const emojiImgPath = "https://npm.elemecdn.com/halo-comment-yu@latest/cdn/img/emoji";
 
 export default [
     new Emoji(`<img alt="smile" referrerpolicy="no-referrer" class="vemoji" src="${emojiImgPath}/2018new_weixioa02_org.png">`, "", " :smile: ", []),
